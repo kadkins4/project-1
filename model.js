8986faa050a20e8d1e6dc6b219ed770cc835d029
Kendall <adkins.kendall90@gmail.com>
2017-07-12 11:43:07 -0400

    logic added

--- model.js
+++ model.js
@@ -4,8 +4,6 @@
 // As a player, I want challenging questions to challenge my friends
 // on their random music, travel, and game knowledge
 
-// NEXT STEPS: RANDOM NUMBER GENERATOR NEEDS TO FIRE THE GETQUESTION FUNCTION()
-// WHICH WILL NEED TO GRAB THE THIS.CARDS[I] ARRAY INFO -- FROM THERE CONTINUE
 // TEST AND TRANSITION INTO VISUAL
 
 // logic handler
@@ -35,11 +33,11 @@ class Model {
         3: 'Cuba',
         4: 'Mashed Potatoes'
       }]
-    this.current = 0
+    this.current = 0 // updates with randomQuest
   }
   timer () {
     var count = 60
-    var counter = setInterval(time, 1000)
+    var counter = setInterval(time, 1000) // test good
     function time () {
       count = count - 1
       console.log(count)
@@ -51,16 +49,18 @@ class Model {
     }
   } // close timer
   getQuestion (card) {
-    console.log(`this is this.card[this.current] ${this.cards}[${this.current}]`)
+    console.log('getting question')
+    console.log(`this is this.cards[this.current] ${this.cards}[${this.current}]`)
     return this.cards[this.current]
     // slice or splice selected into current[] --> 2nd phase used[]
   }
   randomQuest () { // look up js array randomizer
-    this.current = Math.floor(Math.random() * this.cards.length)
-    console.log(`this is this.current: ${this.current}`)
-    return this.current
+    console.log('running random question')
+    this.current = Math.floor(Math.random() * this.cards.length) // QUESTION BIND
+    console.log(`changed this.current to: ${this.current}`)
+    this.getQuestion()
   }
-  score (response) { // if correct: runs score + always runs generate and visual response
+  score (response) { // if correct: runs score + always runs visual response & randomQuest
     if (response === this.cards[this.current]['1']) {
       console.log('correct')
     // function visual response (visual)
@@ -69,7 +69,11 @@ class Model {
     // function visual response (visual)
       console.log('wrong')
     }
-    // function generate new question/answer
-    randomQuest()
+    this.randomQuest()
   }
+  // send () {
+  //   console.log(`this.cards[this.current] is: ${this.cards[this.current]}`)
+  //   var shuffled = this.cards[this.current].concat().sort(function () { return 0.5 - Math.random() })
+  //   console.log = (shuffled)
+  // } // QUESTION SEND
 } // close class model
--- visual.js
+++ visual.js
@@ -1,3 +1,11 @@
-/* global $ */
+/* globals $ */
 
-// console.log('2. Visual Ready')
+console.log('2. Visual Ready')
+// do i make one variable for all incorrect?
+var correct = $('.a1')
+var a2 = $('.a2')
+var a3 = $('.a3')
+var a4 = $('.a4')
+var prob = $('.problem')
+var time = $('.time')
+var pts = $('points')
